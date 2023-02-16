fc68b61252088fba0d3bc16f7db8a7307da6fcd4
vspaz <vladimir.edelman@gmail.com>
2023-02-16 20:56:41 +0100

    add tests for multiple constructors

--- src/test/scala/wls/WlsTest.scala
+++ src/test/scala/wls/WlsTest.scala
@@ -5,9 +5,15 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class WlsTest extends AnyFunSuite {
 
-  test("org.vspaz.wls.Point") {
+  test("org.vspaz.wls.PointConstructorOk") {
     val point = new Point(intercept = 10, slope = 20)
     assertEquals(10, point.intercept)
     assertEquals(20, point.slope)
+  }
+
+  test("org.vspaz.wls.WlsConstructorOk") {
+   new Wls(Array(1.0), Array(1.0), 1.0)
+   new Wls(Array(1.0), Array(1.0), Array(1.0))
+   new Wls(Array(1.0), Array(1.0))
   }
 }
