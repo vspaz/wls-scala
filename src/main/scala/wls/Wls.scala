306d814f4e7673d6eec67f6dfbf80b39da1c2b75
vspaz <vladimir.edelman@gmail.com>
2023-02-16 21:02:09 +0100

    add validation

--- src/main/scala/wls/Wls.scala
+++ src/main/scala/wls/Wls.scala
@@ -1,8 +1,13 @@
 package wls
 
 class Wls(val xPoints: Array[Double], val yPoints: Array[Double], val weights: Array[Double]) {
-  def this(xPoints: Array[Double], yPoints: Array[Double], weight: Double) =
+  assert(xPoints.length == yPoints.length)
+  assert(xPoints.length == weights.length)
+  assert(xPoints.length > 2)
+  def this(xPoints: Array[Double], yPoints: Array[Double], weight: Double) = {
     this(xPoints, yPoints, Array.fill(xPoints.length)(weight))
+  }
+
   def this(xPoints: Array[Double], yPoints: Array[Double]) =
-    this(xPoints, yPoints, 1)
+    this(xPoints, yPoints, 1.0)
 }
