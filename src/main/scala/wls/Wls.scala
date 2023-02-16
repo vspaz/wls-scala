e75238947974dcc1a2267d60fddda0b0b3218668
vspaz <vladimir.edelman@gmail.com>
2023-02-16 20:51:55 +0100

    add basic test for wls.Point

--- src/main/scala/wls/Wls.scala
+++ src/main/scala/wls/Wls.scala
@@ -1,7 +1,7 @@
 package wls
 
 class Wls(val xPoints: Array[Double], val yPoints: Array[Double], val weights: Array[Double]) {
-  def this(xPoints: Array[Double], yPoints: Array[Double], weight: Int) =
+  def this(xPoints: Array[Double], yPoints: Array[Double], weight: Double) =
     this(xPoints, yPoints, Array.fill(xPoints.length)(weight))
   def this(xPoints: Array[Double], yPoints: Array[Double]) =
     this(xPoints, yPoints, 1)
--- src/test/scala/wls/WlsTest.scala
+++ src/test/scala/wls/WlsTest.scala
@@ -1,5 +1,13 @@
 package wls
 
-class WlsTest {
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.scalatest.funsuite.AnyFunSuite
 
+class WlsTest extends AnyFunSuite {
+
+  test("org.vspaz.wls.Point") {
+    val point = new Point(intercept = 10, slope = 20)
+    assertEquals(10, point.intercept)
+    assertEquals(20, point.slope)
+  }
 }
