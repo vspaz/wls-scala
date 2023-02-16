0ba36034ca8f50760e35ff649a2c18b2fff0702a
vspaz <vladimir.edelman@gmail.com>
2023-02-16 20:45:12 +0100

    add constructors

--- src/main/scala/wls/Wls.scala
+++ src/main/scala/wls/Wls.scala
@@ -1,5 +1,8 @@
 package wls
 
-class Wls {
-
+class Wls(val xPoints: Array[Double], val yPoints: Array[Double], val weights: Array[Double]) {
+  def this(xPoints: Array[Double], yPoints: Array[Double], weight: Int) =
+    this(xPoints, yPoints, Array.fill(xPoints.length)(weight))
+  def this(xPoints: Array[Double], yPoints: Array[Double]) =
+    this(xPoints, yPoints, 1)
 }
