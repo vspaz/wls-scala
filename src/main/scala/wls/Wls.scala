295daf61e6d88b9ce705c2c98aced7b215a1be2d
vspaz <vladimir.edelman@gmail.com>
2023-02-16 21:09:00 +0100

    add wls implementation

--- src/main/scala/wls/Wls.scala
+++ src/main/scala/wls/Wls.scala
@@ -11,8 +11,42 @@ class Wls(val xPoints: Array[Double], val yPoints: Array[Double], val weights: A
   def this(xPoints: Array[Double], yPoints: Array[Double]) =
     this(xPoints, yPoints, 1.0)
 
-  def fitLinearRegression(): Point = {
+  def fitLinearRegression(): Option[Point] = {
+    var sumOfWeights = 0.0
+    var sumOfWeightsByXSquared = 0.0
+    var sumOfXByYByWeights = 0.0
+    var sumOfXByWeights = 0.0
+    var sumOfYByWeights = 0.0
 
-    new Point(0.0, 0.0)
+    var Xi = 0.0
+    var Yi = 0.0
+    var Wi = 0.0
+    var XiByWi = 0.0
+
+    var i = 0
+    while (i < this.xPoints.length) {
+      Xi = this.xPoints(i)
+      Yi = this.yPoints(i)
+      Wi = this.weights(i)
+      sumOfWeights += Wi
+      XiByWi = Xi * Wi
+      sumOfXByWeights += XiByWi
+      sumOfXByYByWeights += XiByWi * Yi
+      sumOfYByWeights += Yi * Wi
+      sumOfWeightsByXSquared += XiByWi * Xi
+
+      i += 1
+    }
+
+    val dividend = sumOfWeights * sumOfXByYByWeights - sumOfXByWeights * sumOfYByWeights
+    val divisor = sumOfWeights * sumOfWeightsByXSquared - sumOfXByWeights * sumOfXByWeights
+
+    if (divisor == 0)
+      return None
+
+    val slope = dividend / divisor
+    val intercept = (sumOfYByWeights - slope * sumOfXByWeights) / sumOfWeights
+
+    Option(new Point(intercept, slope))
   }
 }
