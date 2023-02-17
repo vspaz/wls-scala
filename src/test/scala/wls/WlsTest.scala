b8eafa9384d5c364e6d801b4496ffa089287e1e5
vspaz <vladimir.edelman@gmail.com>
2023-02-17 09:19:24 +0100

    add test

--- src/test/scala/wls/WlsTest.scala
+++ src/test/scala/wls/WlsTest.scala
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.scalatest.funsuite.AnyFunSuite
 
 class WlsTest extends AnyFunSuite {
+  val delta = 1.0e-6
 
   test("org.vspaz.wls.PointConstructorOk") {
     val point = new Point(intercept = 10, slope = 20)
@@ -12,8 +13,16 @@ class WlsTest extends AnyFunSuite {
   }
 
   test("org.vspaz.wls.WlsConstructorOk") {
-   new Wls(Array(1.0), Array(1.0), 1.0)
-   new Wls(Array(1.0), Array(1.0), Array(1.0))
-   new Wls(Array(1.0), Array(1.0))
+    new Wls(Array(1.0), Array(1.0), 1.0)
+    new Wls(Array(1.0), Array(1.0), Array(1.0))
+    new Wls(Array(1.0), Array(1.0))
+  }
+
+  test("org.vspaz.wls.WlsWithStableWeight") {
+    val x: Array[Double] = Array(1, 2, 3, 4, 5, 6, 7)
+    val y: Array[Double] = Array(1, 3, 4, 5, 2, 3, 4)
+    val point = new Wls(x, y).fitLinearRegression().get
+    assertEquals(2.14285714, point.intercept, delta)
+    assertEquals(0.25, point.slope, delta)
   }
 }
