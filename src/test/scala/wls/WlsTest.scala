e47949ffe27373b4f188027c87fd6111f9f5ff14
vspaz <vladimir.edelman@gmail.com>
2023-02-17 09:22:20 +0100

    add wls.TestWlsModelWithStableWeight

--- src/test/scala/wls/WlsTest.scala
+++ src/test/scala/wls/WlsTest.scala
@@ -12,16 +12,23 @@ class WlsTest extends AnyFunSuite {
     assertEquals(20, point.slope)
   }
 
-  test("org.vspaz.wls.WlsConstructorOk") {
+  test("org.vspaz.wls.TestWlsConstructorOk") {
     new Wls(Array(1.0), Array(1.0), 1.0)
     new Wls(Array(1.0), Array(1.0), Array(1.0))
     new Wls(Array(1.0), Array(1.0))
   }
 
-  test("org.vspaz.wls.WlsWithStableWeight") {
+  test("org.vspaz.wls.TestWlsModelWithStableWeight") {
     val x: Array[Double] = Array(1, 2, 3, 4, 5, 6, 7)
     val y: Array[Double] = Array(1, 3, 4, 5, 2, 3, 4)
     val point = new Wls(x, y).fitLinearRegression().get
+    assertEquals(2.14285714, point.intercept, delta)
+    assertEquals(0.25, point.slope, delta)
+  }
+  test("org.vspaz.wls.TestWlsModelWithSingleWeight") {
+    val x: Array[Double] = Array(1, 2, 3, 4, 5, 6, 7)
+    val y: Array[Double] = Array(1, 3, 4, 5, 2, 3, 4)
+    val point = new Wls(x, y, 0.9).fitLinearRegression().get
     assertEquals(2.14285714, point.intercept, delta)
     assertEquals(0.25, point.slope, delta)
   }
