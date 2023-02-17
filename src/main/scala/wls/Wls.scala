a1cac6fee243b3650e7f7ccfbc54eb1aca5a9b0f
vspaz <vladimir.edelman@gmail.com>
2023-02-17 09:28:03 +0100

    fix an error; add another test

--- src/main/scala/wls/Wls.scala
+++ src/main/scala/wls/Wls.scala
@@ -3,7 +3,7 @@ package wls
 class Wls(val xPoints: Array[Double], val yPoints: Array[Double], val weights: Array[Double]) {
   assert(xPoints.length == yPoints.length)
   assert(xPoints.length == weights.length)
-  assert(xPoints.length > 2)
+  assert(xPoints.length >= 2)
   def this(xPoints: Array[Double], yPoints: Array[Double], weight: Double) = {
     this(xPoints, yPoints, Array.fill(xPoints.length)(weight))
   }
--- src/test/scala/wls/WlsTest.scala
+++ src/test/scala/wls/WlsTest.scala
@@ -18,18 +18,26 @@ class WlsTest extends AnyFunSuite {
     new Wls(Array(1.0), Array(1.0))
   }
 
-  test("org.vspaz.wls.TestWlsModelWithStableWeight") {
+  test("org.vspaz.wls.TestWlsModelWithStableWeightOk") {
     val x: Array[Double] = Array(1, 2, 3, 4, 5, 6, 7)
     val y: Array[Double] = Array(1, 3, 4, 5, 2, 3, 4)
     val point = new Wls(x, y).fitLinearRegression().get
     assertEquals(2.14285714, point.intercept, delta)
     assertEquals(0.25, point.slope, delta)
   }
-  test("org.vspaz.wls.TestWlsModelWithSingleWeight") {
+  test("org.vspaz.wls.TestWlsModelWithSingleWeightOk") {
     val x: Array[Double] = Array(1, 2, 3, 4, 5, 6, 7)
     val y: Array[Double] = Array(1, 3, 4, 5, 2, 3, 4)
     val point = new Wls(x, y, 0.9).fitLinearRegression().get
     assertEquals(2.14285714, point.intercept, delta)
     assertEquals(0.25, point.slope, delta)
+  }
+
+  test("org.vspaz.wls.TestWlsModelHorizontalLineOk") {
+    val x: Array[Double] = Array(0.0, 1.0)
+    val y: Array[Double] = Array(10.0, 10.0)
+    val point = new Wls(x, y).fitLinearRegression().get
+    assertEquals(10.0, point.intercept)
+    assertEquals(0.0, point.slope)
   }
 }
